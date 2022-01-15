1ec9f12497dd3d5fff3d16236447970c492dc427
snzhr <snzhrski@gmail.com>
2022-01-15 14:17:03 +0600

    preparing for implementation

---
+++ src/components/Builder.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="builder">
+    <form class="row g-2">
+      <div class="col-md-6">
+        <label for="fullname" class="form-label">Fullname</label>
+        <input
+          type="email"
+          class="form-control"
+          v-model="user.fullname"
+          id="fullname"
+        />
+      </div>
+      <div class="col-md-6">
+        <label for="position" class="form-label">Job position</label>
+        <input
+          type="email"
+          class="form-control"
+          v-model="user.position"
+          id="position"
+        />
+      </div>
+      <div class="col-12">
+        <button type="button" @click="generate" class="btn btn-primary">
+          Generate PDF
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { jsPDF } from "jspdf";
+export default {
+  data() {
+    return {
+      user: {
+        fullname: "",
+        position: "",
+      },
+    };
+  },
+  methods: {
+    generate() {
+      const doc = new jsPDF();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Builder />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Builder from "../components/Builder.vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { Builder },
+};
 </script>
